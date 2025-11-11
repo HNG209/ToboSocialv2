3bc65079e0e60069904e70c815519344053a2f43
Hung hay ho <hungok2909@gmail.com>
2025-11-11 23:12:17 +0700

    minor changes

--- src/axios/axios.customize.js
+++ src/axios/axios.customize.js
@@ -3,7 +3,6 @@ import axios from "axios";
 const instance = axios.create({
   baseURL: import.meta.env.VITE_BACKEND_URL,
   headers: {
-    "Content-Type": "application/json",
     "ngrok-skip-browser-warning": "true",
   },
   withCredentials: true, // nếu dùng cookie refreshToken
